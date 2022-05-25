f56713d27f9dcef9f3feeddf0b5659ef0c7c9a2d
Remco Bloemen <remco@wicked.ventures>
2022-05-25 14:34:55 -0700

    Test allocator confs

---
+++ tests/main.rs
@@ -0,0 +1,34 @@
+#![doc = include_str!("../Readme.md")]
+#![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
+
+use cli_batteries::{run, Version};
+use std::{io::Result, path::PathBuf};
+use structopt::StructOpt;
+use tokio::fs::File;
+
+const MOCK_VERSION: Version = Version {
+    pkg_name:     "cli-test",
+    pkg_version:  "v0.0.0",
+    pkg_repo:     "https://github.com/recmo/cli-batteries",
+    crate_name:   "test",
+    commit_hash:  "7cdd3615368b7e2ed1e053f33628fe7f65e6a538",
+    long_version: "v0.0.0 First release",
+    target:       "aarch64-apple-darwin",
+};
+
+#[derive(StructOpt)]
+struct Options {
+    /// File to read
+    #[structopt(long, env, default_value = "Readme.md")]
+    file: PathBuf,
+}
+
+async fn app(options: Options) -> Result<()> {
+    let mut file = File::open(options.file).await?;
+    Ok(())
+}
+
+#[test]
+fn main() {
+    cli_batteries::run(MOCK_VERSION, app);
+}
