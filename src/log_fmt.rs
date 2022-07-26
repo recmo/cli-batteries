6d812ffb8c0109b916186cd940d6a21b27e0d79a
Remco Bloemen <remco@wicked.ventures>
2022-07-25 22:28:21 -0700

    Rewrite span events

--- src/log_fmt.rs
+++ src/log_fmt.rs
@@ -1,11 +1,11 @@
 use ansi_term::{Colour, Style};
 use itertools::Itertools;
 use std::{
-    fmt::{Debug, Result},
+    fmt::{Debug, Result, Error},
     time::Instant,
 };
 use tracing::{
-    field::{Field, Visit},
+    field::{Field, Visit, FieldSet, display},
     Event, Level, Subscriber,
 };
 use tracing_log::NormalizeEvent;
@@ -14,6 +14,7 @@ use tracing_subscriber::{
     fmt::{format::Writer, FmtContext, FormatEvent, FormatFields},
     registry::LookupSpan,
 };
+use tracing::Value;
 
 pub struct LogFmt {
     epoch: Instant,
@@ -62,39 +63,69 @@ where
         write!(writer, "{}", bold.suffix())?;
 
         if meta.is_span() {
-            // Heuristic to detect open or close span
-            let open = !event
-                .fields()
-                .find(|field| field.name() == "time.busy")
-                .is_some();
+            // Extract fields from event
+            #[derive(Debug,Default)]
+            struct Visitor{
+                time_busy: Option<String>,
+                time_idle: Option<String>,
+            };
+            impl Visit for Visitor {
+                fn record_debug(&mut self, field: &Field, value: &dyn Debug) {
+                    match field.name() {
+                        "time.busy" => self.time_busy = Some(format!("{:?}", value)),
+                        "time.idle" => self.time_idle = Some(format!("{:?}", value)),
+                        _ => (),
+                    }
+                }
+            }
+            let mut visitor = Visitor::default();
+            event.record(&mut visitor);
 
-            let name = event
+            // Get fields
+            let callsite = meta.callsite();
+            let span = event
                 .parent()
                 .and_then(|id| ctx.span(id))
-                .map_or("unknown", |span| span.name());
+                .ok_or(Error::default())?;
+            let message = span.name();
+            if let (Some(time_busy), Some(time_idle)) = (visitor.time_busy, visitor.time_idle) {
+                // Closing event
+                let time_busy = display(time_busy);
+                let time_idle = display(time_idle);
+                let span = display("end");
+                let field_set = FieldSet::new(&[
+                    "message",
+                    "span",
+                    "time.busy",
+                    "time.idle",
+                    ], callsite);
+                let mut fields = field_set.iter();
+                let values = [
+                    (&fields.next().unwrap(), Some(&message as  &dyn Value)),
+                    (&fields.next().unwrap(), Some(&span)),
+                    (&fields.next().unwrap(), Some(&time_busy)),
+                    (&fields.next().unwrap(), Some(&time_idle))
+                ];
+                let value_set = field_set.value_set(&values);
+                let event = Event::new_child_of(event.parent().cloned(), event.metadata(), &value_set);
+                ctx.format_fields(writer.by_ref(), event);
+            } else {
+                // Opening event
+                let span = display("begin");
+                let field_set = FieldSet::new(&["message", "span"], callsite);
+                let mut fields = field_set.iter();
+                let values = [
+                    (&fields.next().unwrap(), Some(&message as  &dyn Value)),
+                    (&fields.next().unwrap(), Some(&span))
+                ];
+                let value_set = field_set.value_set(&values);
+                let event = Event::new_child_of(event.parent().cloned(), event.metadata(), &value_set);
+                ctx.format_fields(writer.by_ref(), event);
+            }
 
-            // Create event with augmented message
-            let values = event.fields().map(|field| {
-                match field.name() {
-                    _ => 
-                }
-            }).collect::<Vec<_>>();
-            let vs = fs.value_set(&v);
-            let event = Event::new_child_of(event.parent(), meta, &vs);
-
+        } else {
+            ctx.format_fields(writer.by_ref(), event);
         }
-
-        // struct Visitor<'a>(Writer<'a>);
-        // impl<'a> Visit for Visitor<'a> {
-        //     fn record_debug(&mut self, field: &Field, value: &dyn Debug) {
-        //         write!(self.0, "{} = {:?}", field, value);
-        //     }
-        // }
-        // let mut visitor = Visitor(writer);
-        // event.record(&mut visitor);
-        // let mut writer = visitor.0;
-
-        ctx.format_fields(writer.by_ref(), event);
 
         writeln!(writer)?;
 
