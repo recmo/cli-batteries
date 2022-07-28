98d057fb57e7dc936e20be9c537604d4964fb1ce
Remco Bloemen <remco@wicked.ventures>
2022-07-28 14:10:58 -0700

    Release v0.3.2

--- example/src/main.rs
+++ example/src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use cli_batteries::version;
 use std::{io::Result, path::PathBuf};
 use tokio::{fs::File, io::AsyncReadExt};
-use tracing::{event, instrument, span, Level};
+use tracing::{event, info_span, instrument, Instrument, Level};
 
 #[derive(Clone, Debug, Parser)]
 struct Options {
@@ -17,14 +17,11 @@ struct Options {
 #[instrument(name = "Example app")]
 async fn app(options: Options) -> Result<()> {
     let mut file = File::open(options.file.clone()).await?;
-    {
-        let span = span!(Level::INFO, "Reading file", file=?options.file);
-        let _ = span.enter();
-
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).await?;
-        event!(Level::INFO, length = contents.len(), "Read file");
-    }
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .instrument(info_span!("Reading file", file=?options.file))
+        .await?;
+    event!(Level::INFO, length = contents.len(), "Read file");
     Ok(())
 }
 
