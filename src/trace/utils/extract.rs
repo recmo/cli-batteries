46ea818ef6a3b2ebf1a1def24522d4c73115246e
Dzejkop <jakubtrad@gmail.com>
2023-05-18 20:17:27 +0200

    Handle zero trace ids

--- src/trace/utils/extract.rs
+++ src/trace/utils/extract.rs
@@ -18,9 +18,21 @@ where
     let extensions = span_ref.extensions();
 
     let data = extensions.get::<OtelData>()?;
-    let id = data.parent_cx.span().span_context().trace_id();
+    let parent_trace_id = data.parent_cx.span().span_context().trace_id();
+    let parent_trace_id_u128 = u128::from_be_bytes(parent_trace_id.to_bytes());
 
-    Some(u128::from_be_bytes(id.to_bytes()))
+    // So parent trace id will usually be zero UNLESS we extract a trace id from
+    // headers in which case it'll be the trace id from headers. And for some
+    // reason this logic is not handled with Option
+    //
+    // So in case the parent trace id is zero, we should use the builder trace id.
+    if parent_trace_id_u128 == 0 {
+        let builder_id = data.builder.trace_id?;
+
+        Some(u128::from_be_bytes(builder_id.to_bytes()))
+    } else {
+        Some(parent_trace_id_u128)
+    }
 }
 
 /// Finds Otel span id
@@ -40,9 +52,17 @@ where
     let extensions = span_ref.extensions();
 
     let data = extensions.get::<OtelData>()?;
-    let id = data.parent_cx.span().span_context().span_id();
+    let parent_span_id = data.parent_cx.span().span_context().span_id();
+    let parent_span_id_u64 = u64::from_be_bytes(parent_span_id.to_bytes());
 
-    Some(u64::from_be_bytes(id.to_bytes()))
+    // Same logic as for trace ids
+    if parent_span_id_u64 == 0 {
+        let builder_id = data.builder.span_id?;
+
+        Some(u64::from_be_bytes(builder_id.to_bytes()))
+    } else {
+        Some(parent_span_id_u64)
+    }
 }
 
 fn span_from_ctx<'a, S, N>(ctx: &'a FmtContext<'a, S, N>) -> Option<SpanRef<'a, S>>
